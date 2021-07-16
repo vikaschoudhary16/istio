9039d879c3ed6af452fd19e622a333b7d345dbba
Suchith J N <41095737+su225@users.noreply.github.com>
2021-07-16 19:36:08 +0530

    handle NodePort services with external traffic policy set to Local (#386) (#387)
    * handle NodePort services with external traffic policy set to Local
    
    * add tests and some more refinement,refactors etc
    
    * address PR feedback
    
    (cherry picked from commit 7b379373268481f137114812fa265a11efb48bba)
    (cherry picked from commit d7de3c2f7f10144d7f6bef69b4d08d3fcb4141ab)

--- pilot/pkg/serviceregistry/kube/controller/controller.go
+++ pilot/pkg/serviceregistry/kube/controller/controller.go
@@ -267,6 +267,12 @@ type Controller struct {
 	externalNameSvcInstanceMap map[host.Name][]*model.ServiceInstance
 	// index over workload instances from workload entries
 	workloadInstancesIndex workloadinstances.Index
+	// serviceToWorkloadNodesMap is a mapping of NodePort services to the list of kubernetes node names
+	// on which there is at least one workload belonging to the service is running. This is needed to handle
+	// node-port services with external traffic policy type set to Local (default is Cluster). This is because
+	// when it is set to local and traffic is sent to a node not hosting a workload, it will be dropped. This
+	// happens intermittently based on the node which takes the traffic.
+	serviceToWorkloadNodesMap map[host.Name]map[string]struct{}
 
 	multinetwork
 	// beginSync is set to true when calling SyncAll, it indicates the controller has began sync resources.
@@ -287,6 +293,7 @@ func NewController(kubeClient kubelib.Client, options Options) *Controller {
 		nodeInfoMap:                make(map[string]kubernetesNode),
 		externalNameSvcInstanceMap: make(map[host.Name][]*model.ServiceInstance),
 		workloadInstancesIndex:     workloadinstances.NewIndex(),
+		serviceToWorkloadNodesMap:  make(map[host.Name]map[string]struct{}),
 		beginSync:                  atomic.NewBool(false),
 		initialSync:                atomic.NewBool(false),
 
@@ -542,6 +549,7 @@ func (c *Controller) deleteService(svc *model.Service) {
 	delete(c.externalNameSvcInstanceMap, svc.Hostname)
 	_, isNetworkGateway := c.networkGatewaysBySvc[svc.Hostname]
 	delete(c.networkGatewaysBySvc, svc.Hostname)
+	delete(c.serviceToWorkloadNodesMap, svc.Hostname)
 	c.Unlock()
 
 	if isNetworkGateway {
@@ -631,6 +639,12 @@ func (c *Controller) onNodeEvent(_, obj any, event model.Event) error {
 		updatedNeeded = true
 		c.Lock()
 		delete(c.nodeInfoMap, node.Name)
+
+		// We should remove the node for all the services that
+		// had a workload there as we don't want to route traffic
+		for h := range c.serviceToWorkloadNodesMap {
+			delete(c.serviceToWorkloadNodesMap[h], node.Name)
+		}
 		c.Unlock()
 	} else {
 		k8sNode := kubernetesNode{labels: node.Labels}
