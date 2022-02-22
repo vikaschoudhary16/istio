f0e86a2313239e15c6c96d5ce70c527e9b12fc33
Yaroslav Skopets <yaroslav@tetrate.io>
2022-02-22 11:26:07 +0100

    ci: set up CI for `Istio 1.12` (no integration tests) (#232)
    Signed-off-by: Yaroslav Skopets <yaroslav@tetrate.io>

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -38,7 +38,7 @@ BUILDER_SHA=7345a6eb03119c7dd57c7862ab53fdf99d574325
 # This will create a version like 1.4-alpha.sha
 NEXT_VERSION=1.12
 TAG=$(git rev-parse HEAD)
-VERSION="${NEXT_VERSION}-alpha.${TAG}"
+VERSION="${VERSION:-${NEXT_VERSION}-alpha.${TAG}}"
 
 # In CI we want to store the outputs to artifacts, which will preserve the build
 # If not specified, we can just create a temporary directory
@@ -99,7 +99,13 @@ release-builder build --manifest <(echo "${MANIFEST}")
 release-builder validate --release "${WORK_DIR}/out"
 
 if [[ -z "${DRY_RUN:-}" ]]; then
-  release-builder publish --release "${WORK_DIR}/out" \
-    --gcsbucket "${GCS_BUCKET}" --gcsaliases "${NEXT_VERSION}-dev" \
-    --dockerhub "${DOCKER_HUB}" --dockertags "${VERSION},${NEXT_VERSION}-dev"
+  read -ra PUBLISH_OPTIONS <<< "${PUBLISH_OPTIONS:-}"
+
+  [[ "${PUBLISH_GCS:-}" != "0" ]] && PUBLISH_OPTIONS+=(--gcsbucket "${GCS_BUCKET}")
+
+  [[ "${PUBLISH_GCS:-}" != "0" && "${PUBLISH_GCS_ALIASES:-}" != "0" ]] && PUBLISH_OPTIONS+=(--gcsaliases "${NEXT_VERSION}-dev")
+
+  [[ "${PUBLISH_DOCKER:-}" != "0" ]] && PUBLISH_OPTIONS+=(--dockerhub "${DOCKER_HUB}" --dockertags "${VERSION},${NEXT_VERSION}-dev")
+
+  release-builder publish --release "${WORK_DIR}/out" "${PUBLISH_OPTIONS[@]}"
 fi
