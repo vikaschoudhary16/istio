fb08329c2cc2b478674bafbd85c3c2b2d271ef43
Aniket Singh <aniketsingh.sachan@gmail.com>
2022-10-14 18:34:32 +0530

    Add back the EnableK8SServiceSelectWorkloadEntries flag
    (cherry picked from commit 748a51bac5eef6950fdd9949a40f8e3aec48e102)

--- pilot/pkg/serviceregistry/kube/controller/network.go
+++ pilot/pkg/serviceregistry/kube/controller/network.go
@@ -15,6 +15,7 @@
 package controller
 
 import (
+	"istio.io/istio/pilot/pkg/features"
 	"net"
 	"strconv"
 	"sync"
@@ -439,8 +440,10 @@ func (c *Controller) updateServiceNodePortAddresses(svcs ...*model.Service) bool
 		if c.isNodePortGatewayService(svc) && svc.Attributes.ExternalTrafficPolicy == model.ExternalTrafficPolicyLocal {
 			endpoints := c.endpoints.buildIstioEndpointsWithService(
 				svc.Attributes.Name, svc.Attributes.Namespace, svc.Hostname, true)
-			fep := c.collectWorkloadInstanceEndpoints(svc)
-			endpoints = append(endpoints, fep...)
+			if features.EnableK8SServiceSelectWorkloadEntries {
+				fep := c.collectWorkloadInstanceEndpoints(svc)
+				endpoints = append(endpoints, fep...)
+			}
 			nodesWithEndpoints := getWorkloadNodeLocations(endpoints)
 			log.Debugf("---> svc:%s nodesWithEndpoints = %#v", svc.Hostname, nodesWithEndpoints)
 			c.Lock()
