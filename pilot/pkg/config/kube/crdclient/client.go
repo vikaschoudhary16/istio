be94ed469292f10e5ec99fa5fc89c928318fcc55
tanujd11 <dwiveditanuj41@gmail.com>
2023-10-26 15:08:02 +0530

    lint and UT fixes
    Signed-off-by: tanujd11 <dwiveditanuj41@gmail.com>

--- pilot/pkg/config/kube/crdclient/client.go
+++ pilot/pkg/config/kube/crdclient/client.go
@@ -89,11 +89,11 @@ type Client struct {
 }
 
 type Option struct {
-	Revision         string
-	DomainSuffix     string
-	Identifier       string
-	NamespacesFilter func(obj interface{}) bool
-	FiltersByGVK     map[config.GroupVersionKind]kubetypes.Filter
+	Revision           string
+	DomainSuffix       string
+	Identifier         string
+	NamespacesFilter   func(obj interface{}) bool
+	FiltersByGVK       map[config.GroupVersionKind]kubetypes.Filter
 	DiscoveryRevisions sets.Set[string]
 }
 
@@ -115,18 +115,18 @@ func NewForSchemas(client kube.Client, opts Option, schemas collection.Schemas) 
 		schemasByCRDName[name] = s
 	}
 	out := &Client{
-		domainSuffix:     opts.DomainSuffix,
-		schemas:          schemas,
-		schemasByCRDName: schemasByCRDName,
-		revision:         opts.Revision,
-		queue:            queue.NewQueue(1 * time.Second),
-		kinds:            map[config.GroupVersionKind]kclient.Untyped{},
-		handlers:         map[config.GroupVersionKind][]model.EventHandler{},
-		client:           client,
+		domainSuffix:       opts.DomainSuffix,
+		schemas:            schemas,
+		schemasByCRDName:   schemasByCRDName,
+		revision:           opts.Revision,
+		queue:              queue.NewQueue(1 * time.Second),
+		kinds:              map[config.GroupVersionKind]kclient.Untyped{},
+		handlers:           map[config.GroupVersionKind][]model.EventHandler{},
+		client:             client,
 		discoveryRevisions: opts.DiscoveryRevisions,
-		logger:           scope.WithLabels("controller", opts.Identifier),
-		namespacesFilter: opts.NamespacesFilter,
-		filtersByGVK:     opts.FiltersByGVK,
+		logger:             scope.WithLabels("controller", opts.Identifier),
+		namespacesFilter:   opts.NamespacesFilter,
+		filtersByGVK:       opts.FiltersByGVK,
 	}
 
 	for _, s := range out.schemas.All() {
--- pilot/pkg/xds/ep_filters.go
+++ pilot/pkg/xds/ep_filters.go
@@ -44,7 +44,7 @@ func (b *EndpointBuilder) EndpointsByNetworkFilter(endpoints []*LocalityEndpoint
 	}
 
 	isSniDnatCluster := model.IsDNSSrvSubsetKey(b.clusterName)
-	
+
 	// A new array of endpoints to be returned that will have both local and
 	// remote gateways (if any)
 	filtered := make([]*LocalityEndpoints, 0)
