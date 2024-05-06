409e9f3d5b3177b08b91242ee79632cd7be9b04c
Yaroslav Skopets <yaroslav@tetrate.io>
2024-05-07 01:18:11 +0530

    ci: add GHA build
    (cherry picked from commit d4619bf3631cbe6af4757ec0a6de36a579bce432)

--- prow/release-commit.sh
+++ prow/release-commit.sh
@@ -32,7 +32,7 @@ DOCKER_HUB=${DOCKER_HUB:-gcr.io/istio-testing}
 GCS_BUCKET=${GCS_BUCKET:-istio-build/dev}
 
 # Enable emulation required for cross compiling a few images (VMs)
-docker run --rm --privileged "${DOCKER_HUB}/qemu-user-static" --reset -p yes
+docker run --rm --privileged gcr.io/istio-testing/qemu-user-static --reset -p yes
 export ISTIO_DOCKER_QEMU=true
 
 # Use a pinned version in case breaking changes are needed
@@ -42,7 +42,7 @@ BUILDER_SHA=7077025726f52330bbdfafb39118acc544aebada
 # This will create a version like 1.4-alpha.sha
 NEXT_VERSION=$(cat "${ROOT}/VERSION")
 TAG=$(git rev-parse HEAD)
-VERSION="${NEXT_VERSION}-alpha.${TAG}"
+VERSION="${VERSION:-${NEXT_VERSION}-alpha.${TAG}}"
 
 # In CI we want to store the outputs to artifacts, which will preserve the build
 # If not specified, we can just create a temporary directory
@@ -103,7 +103,13 @@ release-builder build --manifest <(echo "${MANIFEST}")
 release-builder validate --release "${WORK_DIR}/out"
 
 if [[ -z "${DRY_RUN:-}" ]]; then
-  release-builder publish --release "${WORK_DIR}/out" \
-    --gcsbucket "${GCS_BUCKET}" --gcsaliases "${TAG},${NEXT_VERSION}-dev" \
-    --dockerhub "${DOCKER_HUB}" --dockertags "${TAG},${VERSION},${NEXT_VERSION}-dev"
+  read -ra PUBLISH_OPTIONS <<< "${PUBLISH_OPTIONS:-}"
+
+  [[ "${PUBLISH_GCS:-}" != "0" ]] && PUBLISH_OPTIONS+=(--gcsbucket "${GCS_BUCKET}")
+
+  [[ "${PUBLISH_GCS:-}" != "0" && "${PUBLISH_GCS_ALIASES:-}" != "0" ]] && PUBLISH_OPTIONS+=(--gcsaliases "${NEXT_VERSION}-dev")
+
+  [[ "${PUBLISH_DOCKER:-}" != "0" ]] && PUBLISH_OPTIONS+=(--dockerhub "${DOCKER_HUB}" --dockertags "${VERSION},${NEXT_VERSION}-dev")
+
+  release-builder publish --release "${WORK_DIR}/out" "${PUBLISH_OPTIONS[@]}"
 fi
