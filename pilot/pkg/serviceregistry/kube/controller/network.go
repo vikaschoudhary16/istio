24c7562e0d36efd320137a9ba1dac8c1aa5f6f00
tanujd11 <dwiveditanuj41@gmail.com>
2024-05-16 12:19:53 +0530

    Handle case when ClusterExternalAddresses attribute is nil
    (cherry picked from commit 26afa24c6539577bd950b5dbc9764658ad6c0ad5)

--- pilot/pkg/serviceregistry/kube/controller/network.go
+++ pilot/pkg/serviceregistry/kube/controller/network.go
@@ -486,6 +486,9 @@ func (c *Controller) updateClusterExternalAddressesForNodePortServices(nodeSelec
 			}
 			extAddresses = append(extAddresses, n.address)
 		}
+		if svc.Attributes.ClusterExternalAddresses == nil {
+			svc.Attributes.ClusterExternalAddresses = &model.AddressMap{}
+		}
 		svc.Attributes.ClusterExternalAddresses.SetAddressesFor(c.Cluster(), extAddresses)
 	} else {
 		var nodeAddresses []string
@@ -497,6 +500,9 @@ func (c *Controller) updateClusterExternalAddressesForNodePortServices(nodeSelec
 				}
 				nodeAddresses = append(nodeAddresses, n.address)
 			}
+		}
+		if svc.Attributes.ClusterExternalAddresses == nil {
+			svc.Attributes.ClusterExternalAddresses = &model.AddressMap{}
 		}
 		svc.Attributes.ClusterExternalAddresses.SetAddressesFor(c.Cluster(), nodeAddresses)
 	}
